7e25ef404c9939839d53137613b6a27b301ffab2
Prakhar Sharma <prakhar@theodin.in>
2026-05-18 16:20:29 +0530

    feat(teams): expose /import, /export, /import-template, /import-logs routes

--- src/routes/v1/team.route.js
+++ src/routes/v1/team.route.js
@@ -2,8 +2,14 @@ import express from 'express';
 import auth from '../../middlewares/auth.js';
 import validate from '../../middlewares/validate.js';
 import requirePermissions from '../../middlewares/requirePermissions.js';
+import { teamUpload } from '../../middlewares/teamUploadMulter.js';
+import {
+  teamsImport as teamsImportRateLimit,
+  teamsExport as teamsExportRateLimit,
+} from '../../middlewares/rateLimiter.js';
 import * as teamValidation from '../../validations/team.validation.js';
 import * as teamController from '../../controllers/team.controller.js';
+import * as teamExcelController from '../../controllers/teamExcel.controller.js';
 
 const router = express.Router();
 
@@ -11,6 +17,35 @@ router
   .route('/')
   .post(auth(), requirePermissions('teams.manage'), validate(teamValidation.createTeamMember), teamController.create)
   .get(auth(), requirePermissions('teams.read'), validate(teamValidation.getTeamMembers), teamController.list);
+
+router.post(
+  '/import',
+  auth(),
+  requirePermissions('teams.manage'),
+  teamsImportRateLimit,
+  teamUpload.single('file'),
+  validate(teamValidation.importTeams),
+  teamExcelController.importExcel
+);
+
+router.get(
+  '/export',
+  auth(),
+  requirePermissions('teams.manage'),
+  teamsExportRateLimit,
+  validate(teamValidation.exportTeams),
+  teamExcelController.exportExcel
+);
+
+router.get('/import-template', auth(), requirePermissions('teams.manage'), teamExcelController.downloadTemplate);
+
+router.get(
+  '/import-logs',
+  auth(),
+  requirePermissions('teams.manage'),
+  validate(teamValidation.listImportLogs),
+  teamExcelController.listImportLogs
+);
 
 router
   .route('/:teamMemberId')
